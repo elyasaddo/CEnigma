04bfadc455c197ee131923c31ced8e1e6cbf594b
Elyas K Addo <eka14@ic.ac.uk>
2015-10-22 20:06:53 +0100

    File transfer

--- src/IOBoard.cpp
+++ src/IOBoard.cpp
@@ -1,2 +1,12 @@
 #include <iostream>
+#include <fstream>
 #include "IOBoard.hpp"
+
+IOBoard::IOBoard(int noOfParams, char** filenames, std::istream input);
+
+void run();
+  private:
+    void encryptLetter(char c);
+    void rotate();
+    // properties
+    vector<Rotor> rotors;
--- src/IOBoard.hpp
+++ src/IOBoard.hpp
@@ -1,6 +1,21 @@
 #ifndef IOBOARD_H
 #define IOBOARD_H
 
+#include <istream>
+#include <vector>
+#include "Rotor.hpp"
 
+class IOBoard{
+
+  public:
+    IOBoard(int noOfParams, char** filenames, std::istream input);
+    void run();
+  private:
+    void encryptLetter(char c);
+    void rotate();
+    // properties
+    vector<Rotor> rotors;
+
+}
 
 #endif
