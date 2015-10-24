bf625e2e1bc2cdc54626eaff16e8f055ad11eb2d
elyasaddo <les_a2do@yahoo.co.uk>
2015-10-24 13:00:26 +0100

    commit

--- tests/testIOBoard.cpp
+++ tests/testIOBoard.cpp
@@ -1,4 +1,21 @@
 #include <iostream>
 #include <fstream>
 #include <memory>
+#include "IOBoard.hpp"
+#include "../src/CharVisitor.hpp"
+#include "../src/Plugboard.hpp"
+#include "../src/Rotor.hpp"
+#include "../src/Reflector.hpp"
 
+int main(int argc, char const *argv[]) {
+
+  shared_ptr<IOBoard> ioBoard(
+                        new IOBoard(4,
+                          {"../rotors/I.rot", "../rotors/I.rot",
+                           "../rotors/I.rot", "../plugboards/I.pb"
+                          },
+                          std::cin));
+  ioBoard.run();
+
+  return 0;
+}
